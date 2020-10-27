ad4b7f13b5e4e22ea63ac950a4bfeaa0b9e41b11
Carl B. Smiley <beeryt@users.noreply.github.com>
2020-10-26 21:18:46 -0700

    Assignment03: add initial boilerplate

---
+++ assignment03/blink.c
@@ -0,0 +1,13 @@
+/*
+ *  File:       blink.c
+ *  Author:     Carl B. Smiley
+ *  Date:       Oct 26 2020
+ *  Course:     Fundamentals of Embedded and Real Time Systems
+ *
+ * Description: blinks LED1 (PA5) at a visible rate using XOR
+ */
+
+
+int main() {
+  return 0;
+}
---
+++ assignment03/morse.c
@@ -0,0 +1,12 @@
+/*
+ *  File:       morse.c
+ *  Author:     Carl B. Smiley
+ *  Date:       Oct 26 2020
+ *  Course:     Fundamentals of Embedded and Real Time Systems
+ *
+ * Description: displays my name using morse code on LED1
+ */
+
+int main() {
+  return 0;
+}
