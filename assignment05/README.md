5c0b8c4d6d659998f7da0c56bc12b77e08290cdd
Carl B. Smiley <beeryt@users.noreply.github.com>
2020-11-20 17:29:32 -0800

    Assignment05: partial work on problem1

--- assignment05/README.md
+++ assignment05/README.md
@@ -10,7 +10,18 @@
   > See [swapPointer.c](swapPointer.c) for implementation of `swap_pointer()`.<br>
   > See [main.c](main.c) for implementation of `problem1_main()`.
 ### 1.a Explain what the `main()` function does in order to setup the input arguments prior to calling the `swap_pointer()` function?
-  > Answer Placeholder
+  > The declaration is `void swap_pointer(void**, void**)` since in order to swap pointers (and not their values) the *address* of the pointers must be passed into the swap function. I chose to use `void*` since it is guaranteed to hold any ***data*** pointer type (this will not work generically for function pointers).
+  >
+  > In the `main()` function in order to call `swap_pointer()` two things are needed:
+  > 1. Get the address of the pointers: `&ptr`
+  > 2. Cast to `void*` type: `(void*)&ptr`
+  >
+  > In the end this looks like `swap_pointer((void*)&ptrA, (void*)ptrB);`
+
+  > In assembly this looks like:
+  > ```asm
+  > Assembly Placeholder
+  > ```
 ### 1.b What are the values in `R0` & `R1` when `swap_pointer()` is called?
   > Answer Placeholder
 ### 1.c Share a screenshot of the local variables inside of `main()` after the function `swap_pointer()` returns showing the values of the pointers and what they are pointing to.
--- assignment05/main.c
+++ assignment05/main.c
@@ -7,6 +7,53 @@
  *  Description:    implementations for "main" function for each problem
  */
 
+#include <assert.h>
+
+// Function declarations of functions used in all problems
+void swap_pointers(void** ptrA, void** ptrB);       // used in problem1
+int divAsm(int a);                                  // used in problem2
+void swapCharsAsm(char a, char b);                  // used in problem3
+void swapPointersAsm(void** ptrA, void** ptrB);     // used in problem4
+
+// Function declarations for "main" function for each problem
+int problem1_main();
+int problem2_main();
+int problem3_main();
+int problem4_main();
+
 int main() {
-    return 0;
+    return problem1_main(); // change which problem "main" function runs here
+}
+
+void swap_pointers(void** ptrA, void** ptrB) {
+    void* tmp = *ptrA;
+    *ptrA = *ptrB;
+    *ptrB = tmp;
+}
+
+int problem1_main() {
+    int A = 1;
+    int B = 2;
+    int *ptrA = &A;
+    int *ptrB = &B;
+
+    int *originalA = ptrA;
+    int *originalB = ptrB;
+
+    swap_pointers((void*)&ptrA, (void*)&ptrB);
+
+    assert(ptrA != ptrB);
+    assert(A == *ptrB);
+    assert(B == *ptrA);
+    assert(originalA == ptrB);
+    assert(originalB == ptrA);
+}
+
+int problem2_main() {
+}
+
+int problem3_main() {
+}
+
+int problem4_main() {
 }
