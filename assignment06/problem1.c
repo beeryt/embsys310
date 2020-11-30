72cbed489371e1a64cef963b1fecfb2638670a9f
Carl B. Smiley <beeryt@users.noreply.github.com>
2020-11-29 21:32:14 -0800

    Assignment06: add project for problem1

---
+++ assignment06/problem1.c
@@ -0,0 +1,13 @@
+/*
+ *  File:           problem1.c
+ *  Author:         Carl B. Smiley
+ *  Date:           Nov 29 2020
+ *  Course:         Fundamentals of Embedded and Real Time Systems
+ *
+ *  Description:    implementation for "main" function for problem1
+ */
+
+#include <stm32l475xx.h>
+
+int main() {
+}
